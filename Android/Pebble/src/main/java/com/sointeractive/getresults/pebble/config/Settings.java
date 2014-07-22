6a45de09c7da0fde9fc7bb035d3ee060c88ee717
mwarchol <mwarchol@sosoftware.pl>
2014-07-22 11:04:57 +0200

    Add IsaaCloud config

---
+++ Android/Pebble/src/main/java/com/sointeractive/getresults/pebble/config/Settings.java
@@ -0,0 +1,10 @@
+package com.sointeractive.getresults.pebble.config;
+
+
+public class Settings {
+
+    // connection
+    public static final String instanceId = "157";
+    public static final String appSecret = "59f8d59eb77eaec6f7ef9f1023815550";
+
+}
