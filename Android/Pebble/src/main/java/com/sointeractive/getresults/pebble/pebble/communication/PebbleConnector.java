eb97dd0797f471ba2d1d7932eb4e8e93ff3a15f7
mwarchol <mwarchol@sosoftware.pl>
2014-07-28 10:46:11 +0200

    Use ConcurrentLinkedQueue instead od LinkedList

--- Android/Pebble/src/main/java/com/sointeractive/getresults/pebble/pebble/communication/PebbleConnector.java
+++ Android/Pebble/src/main/java/com/sointeractive/getresults/pebble/pebble/communication/PebbleConnector.java
@@ -7,46 +7,55 @@ import com.sointeractive.android.kit.PebbleKit;
 import com.sointeractive.android.kit.util.PebbleDictionary;
 import com.sointeractive.getresults.pebble.config.Settings;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Observable;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class PebbleConnector extends Observable {
     private static final String TAG = PebbleConnector.class.getSimpleName();
-    private final LinkedList<PebbleDictionary> sendingQueue = new LinkedList<PebbleDictionary>();
+    private final Queue<PebbleDictionary> sendingQueue = new ConcurrentLinkedQueue<PebbleDictionary>();
     private final Context context;
     public boolean connectionState;
+    public boolean sending = false;
 
     public PebbleConnector(final Context context) {
         this.context = context;
     }
 
-    public synchronized void sendNewDataToPebble(final List<PebbleDictionary> data) {
-        clearSendingQueue();
-        sendDataToPebble(data);
+    public void sendNewDataToPebble(final List<PebbleDictionary> data) {
+        synchronized (sendingQueue) {
+            clearSendingQueue();
+            sendDataToPebble(data);
+        }
     }
 
-    public synchronized void sendDataToPebble(final List<PebbleDictionary> data) {
-        if (isPebbleConnected()) {
-            final boolean wasEmpty = sendingQueue.isEmpty();
-            sendingQueue.addAll(data);
-            if (wasEmpty) {
-                sendNext();
+    public void sendDataToPebble(final List<PebbleDictionary> data) {
+        synchronized (sendingQueue) {
+            if (isPebbleConnected()) {
+                sendingQueue.addAll(data);
+                if (!sending) {
+                    sendNext();
+                }
             }
         }
     }
 
-    public synchronized void sendNext() {
-        if (sendingQueue.isEmpty()) {
-            Log.d(TAG, "Event: Nothing to send, empty sendingQueue");
-        } else {
-            final PebbleDictionary data = sendingQueue.poll();
-            Log.d(TAG, "Action: sending response: " + data.toJsonString());
-            PebbleKit.sendDataToPebble(context, Settings.PEBBLE_APP_UUID, data);
+    public void sendNext() {
+        synchronized (sendingQueue) {
+            if (sendingQueue.isEmpty()) {
+                sending = false;
+                Log.d(TAG, "Event: Nothing to send, empty sendingQueue");
+            } else {
+                sending = true;
+                final PebbleDictionary data = sendingQueue.poll();
+                Log.d(TAG, "Action: sending response: " + data.toJsonString());
+                PebbleKit.sendDataToPebble(context, Settings.PEBBLE_APP_UUID, data);
+            }
         }
     }
 
-    private synchronized void clearSendingQueue() {
+    private void clearSendingQueue() {
         sendingQueue.clear();
     }
 
