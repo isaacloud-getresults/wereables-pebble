320f5dc04261851cfc3e89c123f332e4d62ef31c
mwarchol <mwarchol@sosoftware.pl>
2014-07-31 16:20:13 +0200

    Provide new IsaaCloud SDK version

--- Android/isaacloudandroidsdk/src/main/java/pl/sointeractive/isaacloud/Config.java
+++
@@ -1,8 +0,0 @@
-package pl.sointeractive.isaacloud;
-
-public class Config {
-
-    public static final int PORT = 443;
-    public static final String HOST = "isaacloud.com";
-    public static final int TIMEOUT = 5000;
-}
