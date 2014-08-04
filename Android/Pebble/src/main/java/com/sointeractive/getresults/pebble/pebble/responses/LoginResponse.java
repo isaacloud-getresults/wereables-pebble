0e83bccedd3fe351dea62fddb000c6b0c9f3272d
mwarchol <mwarchol@sosoftware.pl>
2014-08-04 09:45:16 +0200

    Add room name to login response

--- Android/Pebble/src/main/java/com/sointeractive/getresults/pebble/pebble/responses/LoginResponse.java
+++ Android/Pebble/src/main/java/com/sointeractive/getresults/pebble/pebble/responses/LoginResponse.java
@@ -7,12 +7,14 @@ public class LoginResponse implements ResponseItem {
     private final String name;
     private final int points;
     private final int rank;
+    private final String roomName;
     private final int beaconsSize;
 
-    public LoginResponse(final String name, final int points, final int rank, final int beaconsSize) {
+    public LoginResponse(final String name, final int points, final int rank, final String roomName, final int beaconsSize) {
         this.name = name;
         this.points = points;
         this.rank = rank;
+        this.roomName = roomName;
         this.beaconsSize = beaconsSize;
     }
 
@@ -20,6 +22,7 @@ public class LoginResponse implements ResponseItem {
     public PebbleDictionary getData(final int responseType) {
         return new DictionaryBuilder(responseType)
                 .addString(name)
+                .addString(roomName)
                 .addInt(points)
                 .addInt(rank)
                 .addInt(beaconsSize)
